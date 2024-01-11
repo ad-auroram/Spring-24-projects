8502cfcb75b8c8e398dd91860e2108945a9ce66e
Rox <roxseeley@gmail.com>
2024-01-11 15:47:38 -0700

    Added class for queue, wrote Node class.

---
+++ 20/project1-Ladder/Queue.java
@@ -0,0 +1,31 @@
+public class Queue<E> {
+
+    public class Node<E>{
+        public E val;
+        public Node nextVal;
+
+        public Node(E value){
+            this.val=value;
+            this.nextVal=null;
+        }
+
+    }
+
+
+
+    public class List {
+        private Node head; // the first element of the list
+        private Node tail; // the last element of the list
+        private int size;
+
+        public void pushItem(E value) {
+
+        }
+        public void popItem(E value){
+
+        }
+    }
+
+}
+
+
