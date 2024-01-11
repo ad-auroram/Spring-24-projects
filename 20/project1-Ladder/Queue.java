1adbaa2472fd1cc43fd46fc00bb1ef0cba370cab
Rox <roxseeley@gmail.com>
2024-01-11 16:07:41 -0700

    Added methods to List class.

--- 20/project1-Ladder/Queue.java
+++ 20/project1-Ladder/Queue.java
@@ -1,16 +1,15 @@
 public class Queue<E> {
 
-    public class Node<E>{
-        public E val;
+    public class Node<E> {
+        public E data;
         public Node nextVal;
 
-        public Node(E value){
-            this.val=value;
-            this.nextVal=null;
+        public Node(E value) {
+            this.data = value;
+            this.nextVal = null;
         }
 
     }
-
 
 
     public class List {
@@ -18,14 +17,43 @@ public class Queue<E> {
         private Node tail; // the last element of the list
         private int size;
 
-        public void pushItem(E value) {
+        public void enqueueItem(E value) {
+            if (tail != null) {
+                tail.nextVal = new Node<>(value);
+                tail.nextVal = tail;
+            } else {
+                tail.nextVal = new Node<>(value);
+                head = tail;
+            }
 
         }
-        public void popItem(E value){
 
+        public void popItem(E value) {
+            if (head == null) {
+                System.out.println("List is empty");
+            } else {
+                head = head.nextVal;
+            }
         }
+
+        public String printContents() {
+            if (head == null) {
+                return "List in empty!";
+            } else {
+                Node temp = head;
+                while (temp != null) {
+                    System.out.println(temp.data);
+                    temp = temp.nextVal;
+                }
+            }
+            return "Done!";
+        }
+
     }
-
 }
+
+
+
+
 
 
