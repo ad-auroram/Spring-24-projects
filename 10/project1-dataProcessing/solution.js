9e3f1adde21810e6ddd4ab7f49e330b741f6fbe2
Rox <roxseeley@gmail.com>
2024-01-24 11:11:11 -0700

    Setting up functions

---
+++ 10/project1-dataProcessing/solution.js
@@ -0,0 +1,22 @@
+
+function filter(data, predicate){
+    //take an array of data and return a new array with entries based on a predicate
+}
+
+function findLast(data, predicate){
+    //find the last value in an array that meet sthe predicate's condition
+}
+
+function map(data, callback){
+    //create a new array based on input array where each value has been passed through the callback function
+}
+
+function pairIf(data1, data2, predicate){
+    //create a new array based on input arrays, values are paired based on the condition set by the predicate (returns true or false)
+}
+
+function reduce(data1, reducer, initialValue){
+    //takes a dataset and reduces it based on the predicate
+    //example given returned two arrays, sorted numbers into evens and odds
+    //
+}
