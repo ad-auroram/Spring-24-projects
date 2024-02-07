0016aee2337c0ff6b4745f297060d75a6e41b14c
Rox <roxseeley@gmail.com>
2024-02-07 10:32:50 -0700

    Made all the boxes and planned story.

---
+++ 10/project2-mini-applications/madLibs.js
@@ -0,0 +1,7 @@
+/*
+Be wary of these ${adjective} quests posted on the board of the local tavern:
+- Looking for ${noun}-caster. Must be able to turn water into ${liquid}.
+- Adventurers, bring me ${number} kobold ${pNoun}. Will pay in ${food}.
+- When I was ${verb-ing} my clothes, I lost my magic ring at the bottom of the town ${noun2}. Please someone ${verb} it back for me?
+*/
+
