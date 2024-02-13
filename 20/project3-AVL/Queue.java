e82901dfb251444b9adb54d435b0edafb82fd5fc
Rox <roxseeley@gmail.com>
2024-02-13 12:48:39 -0700

    New project starts with code from project 1, but switching to an AVL tree rather than a queue.

---
+++ 20/project3-AVL/Queue.java
@@ -0,0 +1,71 @@
+public class Queue<E> {
+
+    public static class Node<E> {
+        public E data;
+        public Node nextVal;
+
+        public Node(E value) {
+            this.data = value;
+            this.nextVal = null;
+        }
+
+    }
+
+
+    public Node head; // the first element of the list
+    public Node tail; // the last element of the list
+    public int size;
+
+    public void enqueueItem(E value) {
+        if (tail != null) {
+            tail.nextVal = new Node<>(value);
+            tail = tail.nextVal;
+            size ++;
+            if (head == null){
+                head = tail;
+            }
+        } else {
+            head = new Node<>(value);
+            tail = head;
+            size ++;
+        }
+
+    }
+
+    public Node dequeueItem() {
+        if (head == null) {
+            System.out.println("Queue is empty.");
+        } else {
+            Node temp = new Node(head.data);
+            head = head.nextVal;
+            if(head == null) {
+                tail = null;
+            }
+            size --;
+            return temp;
+        }
+        return null;
+    }
+
+    public String printContents() {
+        if (head == null) {
+            return "List in empty!";
+        } else {
+            Node temp = head;
+            while (temp != null) {
+                System.out.println(temp.data);
+                temp = temp.nextVal;
+
+            }
+        }
+        return "Done!";
+    }
+
+}
+
+
+
+
+
+
+
