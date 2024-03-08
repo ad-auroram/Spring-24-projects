27716909681197eee389fa1a7df89416519e6875
Rox <roxseeley@gmail.com>
2024-03-08 16:01:57 -0700

    Changed colors, added ids, and starting on js

---
+++ 10/project4-recipeCards/recipe.js
@@ -0,0 +1,14 @@
+
+/* To-do:
+- Title of recipe: should accept with button or enter
+- Ingredients: should accept with click or enter, display in list div
+- Instructions, same as above
+- store ingradients and instructions in arrays
+-set things to clear with reset button, call weite recipe to file with save
+*/
+
+//buttons and input
+const title = document.getElementById("title");
+const ingredients = document.getElementById("ingr-inpt");
+const instructions = document.getElementById("inst-inpt");
+
