7fa6a372dd7f478ede824f8d78aa97df17cf3e22
Rox <roxseeley@gmail.com>
2024-04-02 13:21:58 -0600

    Added size parameter so we can union by size

--- 20/project6-hex/UnionFind.java
+++ 20/project6-hex/UnionFind.java
@@ -1,10 +1,25 @@
 public class UnionFind {
-    private int[] parents;
+    private int[] Parents;
+    private int[] Size;
 
     public UnionFind(int size){
-        parents= new int[size+1];
-        for (int i=1; i<parents.length; i++){
-            parents[i]=i;
+        Parents= new int[size+1];
+        Size = new int[size+1];
+        for (int i=1; i<Parents.length; i++){
+            Parents[i]=i;
+            Size[i]=1;
         }
+    }
+
+    public int find(int num){
+        if (Parents[num]==num){
+            return num;
+        }
+        Parents[num]=find(Parents[num]);
+        return Parents[num];
+    }
+
+    public int union(int num1, int num2){
+        return num1;
     }
 }
