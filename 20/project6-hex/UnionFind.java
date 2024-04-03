e1ba1b557ff65ff0e0f0b920d2491bc72b1ea3f1
Rox <roxseeley@gmail.com>
2024-04-03 15:22:46 -0600

    Wrote tests for union find.

--- 20/project6-hex/UnionFind.java
+++ 20/project6-hex/UnionFind.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class UnionFind {
     private int[] Parents;
     private int[] Size;
@@ -13,8 +15,10 @@ public class UnionFind {
 
     public int find(int num){
         if (Parents[num]==num){
+            System.out.println(num);
             return num;
         }
+        System.out.println(num);
         Parents[num]=find(Parents[num]);
         return Parents[num];
     }
@@ -32,5 +36,67 @@ public class UnionFind {
             Size[group1]+=Size[group2];
             Parents[group2] = group1;
         }
+    }
+
+    public void printParents(){
+        for (int i=1; i<Parents.length; i++){
+            System.out.println(Parents[i]);
+        }
+        System.out.println();
+    }
+
+    public void printSize(){
+        for (int i=1; i<Size.length; i++){
+            System.out.println(Size[i]);
+        }
+        System.out.println();
+    }
+
+    public static void main( String [ ] args ) {
+        UnionFind union = new UnionFind(5);
+        System.out.println("Printing size of unions:");
+        union.printSize();
+        System.out.println("----------------");
+        System.out.println("Printing parents of each index");
+        union.printParents();
+        System.out.println("----------------");
+
+        System.out.println("Unioning 3 and 4...");
+        union.union(3, 4);
+        System.out.println();
+        System.out.println("Unioning 1 and 5...");
+        union.union(1,5);
+        System.out.println();
+
+        System.out.println("Printing size of unions:");
+        union.printSize();
+        System.out.println("----------------");
+        System.out.println("Printing parents of each index");
+        union.printParents();
+        System.out.println("----------------");
+
+        System.out.println("Finding 5:");
+        System.out.println(union.find(5));
+        System.out.println();
+        System.out.println("Finding 4:");
+        System.out.println(union.find(4));
+        System.out.println();
+        System.out.println("Finding 3:");
+        System.out.println(union.find(3));
+
+        System.out.println("Unioning 5 and 4:");
+        union.union(5,4);
+        System.out.println();
+        System.out.println("Printing size of unions:");
+        union.printSize();
+        System.out.println("----------------");
+        System.out.println("Printing parents of each index");
+        union.printParents();
+        System.out.println("----------------");
+
+        System.out.println("Finding 4:");
+        union.find(4);
+
+
     }
 }
